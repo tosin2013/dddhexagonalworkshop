f80fd101c4ed2265dea4872fda7460b441a6ad72
jeremyrdavis <jeremyrdavis@mac.com>
2025-06-02 08:19:02 +0200

    added Module 2 code

---
+++ 02-Value-Objects/dddhexagonalworkshop-02/src/main/java/dddhexagonalworkshop/conference/attendees/infrastructure/AttendeeDTO.java
@@ -0,0 +1,7 @@
+package dddhexagonalworkshop.conference.attendees.infrastructure;
+
+/**
+ * DTO (Data Transfer Object) for Attendee.  DTOs are not specifically a DDD concept.
+ */
+public record AttendeeDTO(String email) {
+}
