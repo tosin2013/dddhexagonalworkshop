e5030a3aa5579f78c057799c4fbbc0e77af3903b
jeremyrdavis <jeremyrdavis@mac.com>
2025-06-03 06:49:32 +0200

    added code for 01

---
+++ 01-End-to-End-DDD/module-01-code/src/test/java/dddhexagonalworkshop/conference/attendees/domain/aggregates/AttendeeTest.java
@@ -0,0 +1,10 @@
+package dddhexagonalworkshop.conference.attendees.domain.aggregates;
+
+import dddhexagonalworkshop.conference.attendees.domain.services.AttendeeRegistrationResult;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttendeeTest {
+
+}
---
+++ 01-End-to-End-DDD/module-01-code/src/test/java/dddhexagonalworkshop/conference/attendees/persistence/AttendeeEntityTest.java
@@ -0,0 +1,9 @@
+package dddhexagonalworkshop.conference.attendees.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttendeeEntityTest {
+
+}
