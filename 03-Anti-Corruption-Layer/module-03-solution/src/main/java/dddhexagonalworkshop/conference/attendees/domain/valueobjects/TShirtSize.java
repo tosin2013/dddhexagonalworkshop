93493432538a4dc93dd2756d0a2cf2678c7902aa
jeremyrdavis <jeremyrdavis@mac.com>
2025-06-03 11:08:35 +0200

    removed code

--- 03-Anti-Corruption-Layer/module-03-solution/src/main/java/dddhexagonalworkshop/conference/attendees/domain/valueobjects/TShirtSize.java
+++
@@ -1,5 +0,0 @@
-package dddhexagonalworkshop.conference.attendees.domain.valueobjects;
-
-public enum TShirtSize {
-    S, M, L, XL, XXL;
-}
--- 03-Anti-Corruption-Layer/module-03-solution/src/main/java/dddhexagonalworkshop/conference/attendees/infrastructure/AttendeeDTO.java
+++
@@ -1,9 +0,0 @@
-package dddhexagonalworkshop.conference.attendees.infrastructure;
-
-import dddhexagonalworkshop.conference.attendees.domain.valueobjects.Address;
-
-/**
- * DTO (Data Transfer Object) for Attendee.  DTOs are not specifically a DDD concept.
- */
-public record AttendeeDTO(String email, String fullName) {
-}
