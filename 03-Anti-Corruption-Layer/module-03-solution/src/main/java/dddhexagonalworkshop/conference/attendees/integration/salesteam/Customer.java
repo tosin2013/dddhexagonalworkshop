ac422242d7ed3cd8c342017ca4503f8fa47cc69d
jeremyrdavis <jeremyrdavis@mac.com>
2025-06-03 10:30:23 +0200

    added code back

---
+++ 03-Anti-Corruption-Layer/module-03-solution/src/main/java/dddhexagonalworkshop/conference/attendees/integration/salesteam/Customer.java
@@ -0,0 +1,4 @@
+package dddhexagonalworkshop.conference.attendees.integration.salesteam;
+
+public record Customer(String firstName, String lastName, String email, String employer, CustomerDetails customerDetails) {
+}
